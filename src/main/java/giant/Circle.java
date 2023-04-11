4b983ecd2dd37bfeebbc5d1b51324f087b7052e9
ZaqueuCavalcante <zaqueudovale@gmail.com>
2023-04-11 16:53:31 -0300

    Refactor circle draw

--- src/main/java/giant/Circle.java
+++ src/main/java/giant/Circle.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
 
 public abstract class Circle {
     int rings;
@@ -41,5 +42,60 @@ public abstract class Circle {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    int d = 900;
+    int r = d / 2;
+
+    public void draw(Game game) {
+        game.background(220);
+        game.fill(220);
+
+        game.strokeWeight(8);
+        game.stroke(0, 49, 75);
+
+        game.circle(0, 0, d); // M=0
+        game.circle(0, 0, d * 4 / 6); // M=1
+        game.circle(0, 0, d * 2 / 6); // M=2
+
+        game.fill(0, 49, 75);
+        game.circle(0, 0, 10); // Center
+
+        // Slices
+        game.strokeWeight(6);
+
+        float angle = 0;
+        game.line(r * cos(angle), r * sin(angle), -r * cos(angle), -r * sin(angle));
+        angle = 45;
+        game.line(r * cos(angle), r * sin(angle), -r * cos(angle), -r * sin(angle));
+        angle = 90;
+        game.line(r * cos(angle), r * sin(angle), -r * cos(angle), -r * sin(angle));
+        angle = 135;
+        game.line(r * cos(angle), r * sin(angle), -r * cos(angle), -r * sin(angle));
+
+        // Points
+        angle = 45 / 2;
+        int deno = 9;
+        game.fill(0, 49, 75);
+
+        int slice = 0;
+        game.textSize(30);
+        game.textAlign(PConstants.CENTER, PConstants.CENTER);
+        for (int i = 0; i < 8; i++) {
+            game.text(game.circle.table[slice][0], (8 * r / deno) * cos(angle), (8 * r / deno) * sin(angle));
+            game.text(game.circle.table[slice][1], (5 * r / deno) * cos(angle), (5 * r / deno) * sin(angle));
+            game.text(game.circle.table[slice][2], (2.20f * r / deno) * cos(angle), (2.20f * r / deno) * sin(angle));
+
+            slice++;
+            angle = angle + 45;
+        }
+    }
+
+    public static final float sin(float angle) {
+        return PApplet.sin(PApplet.radians(angle));
+    }
+
+    public static final float cos(float angle) {
+        return PApplet.cos(PApplet.radians(angle));
     }
 }
--- src/main/java/giant/Game.java
+++ src/main/java/giant/Game.java
@@ -1,15 +1,9 @@
 package giant;
 
 import processing.core.PApplet;
-import processing.core.PConstants;
 
 public class Game extends PApplet {
     Circle circle;
-
-    int x = 0;
-    int y = 0;
-    int d = 900;
-    int r = d / 2;
 
     public void settings() {
         size(1000, 1000);
@@ -20,48 +14,7 @@ public class Game extends PApplet {
     public void draw() {
         translate(500, 500);
 
-        background(220);
-        fill(220);
-
-        strokeWeight(8);
-        stroke(0, 49, 75);
-
-        circle(x, y, d); // M=0
-        circle(x, y, d * 4 / 6); // M=1
-        circle(x, y, d * 2 / 6); // M=2
-
-        fill(0, 49, 75);
-        circle(x, y, 10); // Center
-
-        // Slices
-        strokeWeight(6);
-
-        float angle = 0;
-        line(r * cos(radians(angle)), r * sin(radians(angle)), -r * cos(radians(angle)), -r * sin(radians(angle)));
-        angle = 45;
-        line(r * cos(radians(angle)), r * sin(radians(angle)), -r * cos(radians(angle)), -r * sin(radians(angle)));
-        angle = 90;
-        line(r * cos(radians(angle)), r * sin(radians(angle)), -r * cos(radians(angle)), -r * sin(radians(angle)));
-        angle = 135;
-        line(r * cos(radians(angle)), r * sin(radians(angle)), -r * cos(radians(angle)), -r * sin(radians(angle)));
-
-        // Points
-        angle = 45 / 2;
-        int deno = 9;
-        fill(0, 49, 75);
-
-        int slice = 0;
-        textSize(30);
-        textAlign(PConstants.CENTER, PConstants.CENTER);
-        for (int i = 0; i < 8; i++) {
-            text(circle.table[slice][0], (8 * r / deno) * cos(radians(angle)), (8 * r / deno) * sin(radians(angle)));
-            text(circle.table[slice][1], (5 * r / deno) * cos(radians(angle)), (5 * r / deno) * sin(radians(angle)));
-            text(circle.table[slice][2], (2.20f * r / deno) * cos(radians(angle)),
-                    (2.20f * r / deno) * sin(radians(angle)));
-
-            slice++;
-            angle = angle + 45;
-        }
+        circle.draw(this);
     }
 
     public void keyPressed() {
